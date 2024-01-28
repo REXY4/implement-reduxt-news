53bec5855b38dde2ed337a71e100007d6a5faee8
M RIZKI ISWANTO <muhammadrzki96@gmail.com>
2024-01-28 22:35:07 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
+'use client'
 import ModalPrimary from "@/components/modal/ModalPrimary";
+import { useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 export default function Home() {
-
+  const [valueNumber, setValueNumber] = useState<number>(0)
   return (
     <main >
       <ModalPrimary/>
+      <Container>
+        <Row style={{
+          margin : "50px auto"
+        }}>
+          <Col className="d-flex justify-content-center">
+            <Button
+            onClick={()=>setValueNumber(valueNumber - 1)}
+            variant="danger" style={{
+              marginRight  :"20px",
+            }}>-</Button>
+              <p>{valueNumber}</p>
+            <Button 
+            onClick={()=>setValueNumber(valueNumber + 1)}
+            variant="success" style={{
+              marginLeft  :"20px",
+            }}>+</Button>
+          </Col>
+        </Row>
+      </Container>
     </main>
   );
 }
